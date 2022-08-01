7e782db185971159a72fdc5614a81a702b7cffa1
ParelZalm <62101020+ParelZalm@users.noreply.github.com>
2022-08-01 23:05:44 +0200

    class based components to functional components and hooks

--- app-todo/src/index.js
+++ app-todo/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
+import AppClass from './Components/AppClass';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <AppClass /> */}
   </React.StrictMode>
 );
 
